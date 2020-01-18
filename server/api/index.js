56bee8ecbcbf068d08497830a8393de1faa8e78e
Hector DelaRosa <hdelaros@gmail.com>
2020-01-17 21:49:03 -0500

    Added CRUD routes for items, carts and users

--- server/api/index.js
+++ server/api/index.js
@@ -3,4 +3,15 @@ const apiRouter = require('express').Router();
 // API ROUTES
 //API ROUTES GO HERE//
 
+apiRouter.use('/items', require('./items'));
+apiRouter.use('/users', require('./users'));
+apiRouter.use('/carts', require('./carts'));
+
+apiRouter.use((req, res, next) => {
+  const err = new Error('API route not found');
+  console.error(err);
+  err.status = 404;
+  next(err);
+});
+
 module.exports = apiRouter;
