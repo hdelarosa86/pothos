270e233fbb0596b4580461529579b07957147500
Deanna Wong <dwong221@gmail.com>
2020-01-29 15:11:35 -0500

    attempt #0 at deploy

---
+++ run-seed.js
@@ -0,0 +1,14 @@
+const seed = require("./seed.js");
+const { db } = require("./server/db")
+
+db.sync()
+    .then(seed)
+    .then(() => {
+        console.log(chalk.green("Seed script ran successfully"));
+        process.exit(0);
+    })
+    .catch(e => {
+        console.log(chalk.red("Seed script failed to run"));
+        console.error(e);
+        process.exit(1);
+    })
--- server/db/database.js
+++ server/db/database.js
@@ -6,7 +6,7 @@ const dbName = process.env.NODE_ENV === "test" ? `${pkg.name}-test` : pkg.name;
 
 console.log(chalk.yellow(`Opening database connection to ${dbName}`));
 
-const db = new Sequelize(`postgres://localhost:5432/${dbName}`, {
+const db = new Sequelize(process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`, {
   logging: false
 });
 
