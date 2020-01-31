3321cf990176a472b1ed390eceb84711385b0b74
Hector DelaRosa <hdelaros@gmail.com>
2020-01-31 14:53:17 -0500

    changed <a> tag to <Link>

--- client/Components/NavBar.js
+++ client/Components/NavBar.js
@@ -48,9 +48,9 @@ class NavBar extends React.Component {
                       <Link to="/dashboard">Dashboard</Link>
                     </li>
                     <li>
-                      <a href={"/logout"} onClick={this.props.userLogOut}>
+                      <Link to="/" onClick={this.props.userLogOut}>
                         Logout
-                      </a>
+                      </Link>
                     </li>
                   </div>
                 ) : (
