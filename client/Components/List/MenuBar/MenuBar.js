ed8f35f9fb51cab6fa3b361df874966f49151029
Thompson Harris <william.thompson.harris@gmail.com>
2020-01-31 14:58:24 -0500

    adjusted file for travis

--- client/Components/List/MenuBar/MenuBar.js
+++ client/Components/List/MenuBar/MenuBar.js
@@ -2,11 +2,16 @@ import React from "react";
 //utils
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+//actions
+import { selectFilter } from "../../../Redux/Nav/actions/nav.actions";
 
 /*
-The MenuBar component excepts 2 arguments:
+The MenuBar component excepts 5 arguments:
+type: String +this is used in the link url redirect
 pagination: Boolean +Pulls pagination argument from parent list component
 filterMethods: Array +Pulls filter methods from parent list component
+perPage: Num +uses perPage num to calulate appropriate number of available links
+count: Num +uses count num to calulate appropriate number of available links
 
 onMount the component ....
 -> displays available pages with links if pagination is true
@@ -14,29 +19,44 @@ onMount the component ....
 */
 
 export class MenuBar extends React.Component {
+  pages = () => {
+    return Array(Math.ceil(this.props.count / this.props.perPage))
+      .fill(0)
+      .map((val, idx) => (val = idx + 1));
+  };
+
   render() {
-    const pageArray = [];
-    if (this.props.pages) {
-      for (let i = 1; i < this.props.pages + 1; i++) {
-        pageArray.push(i);
-      }
-    }
     return (
-      <div className='listComponentMenuBar'>
-        <div className='listComponentMenuBar_pages'>
+      <div className="listComponentMenuBar">
+        <div className="listComponentMenuBar_pages">
           {this.props.pagination ? `PAGES :` : null}
           {this.props.pagination
-            ? pageArray.map(num => (
+            ? this.pages().map(num => (
                 <span>
-                  <Link to={`/shop/pages/${num}`}>{num} - </Link>
+                  <Link to={`/${this.props.type}/pages/${num}`}>{num} - </Link>
                 </span>
               ))
             : null}
         </div>
-        <div className='listComponentMenuBar_filters'>
+        <div className="listComponentMenuBar_filters">
           {this.props.filterMethods ? `FILTER :` : null}
           {this.props.filterMethods
-            ? this.props.filterMethods.map(method => <span>{method}</span>)
+            ? this.props.filterMethods.map(method => {
+                return (
+                  <span>
+                    {!method ? (
+                      <button onClick={() => this.props.selectFilter(method)}>
+                        NO FILTER
+                      </button>
+                    ) : (
+                      <button onClick={() => this.props.selectFilter(method)}>
+                        {" "}
+                        {method}{" "}
+                      </button>
+                    )}
+                  </span>
+                );
+              })
             : null}
         </div>
       </div>
@@ -44,8 +64,8 @@ export class MenuBar extends React.Component {
   }
 }
 
-const mapStateToProps = state => ({
-  pages: state.inventory.pages
+const mapDispatchToprops = dispatch => ({
+  selectFilter: filter => dispatch(selectFilter(filter))
 });
 
-export default connect(mapStateToProps)(MenuBar);
+export default connect(null, mapDispatchToprops)(MenuBar);
