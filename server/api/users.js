798081676db1b3e00471c47487c0434b693a8fa3
Hector DelaRosa <hdelaros@gmail.com>
2020-02-01 00:24:33 -0500

    Added admin/user/guest priv to routes

--- server/api/users.js
+++ server/api/users.js
@@ -3,51 +3,97 @@ const app = express();
 const { User, Order } = require("../db/index");
 const chalk = require("chalk");
 
+//middleware to verify guest, user or admin privileges
+app.use((req, res, next) => {
+  if (!req.user) {
+    req.user = {};
+    req.user.guest = true;
+    req.user.id = req.cookies.sessionId;
+  }
+  if (!req.user.admin) {
+    req.adminAuth = false;
+    next();
+  } else {
+    req.adminAuth = true;
+    next();
+  }
+});
+
 app.get("/", (req, res, next) => {
-  User.findAll({ include: [{ model: Order }] })
-    .then(users => res.status(200).send(users))
-    .catch(err => next(err));
-  // if (!req.user.admin) {
-  //   const err = new Error(chalk.redBright("Not Authorized"));
-  //   console.error(err);
-  //   res.status(401).redirect("/");
-  // } else {}
+  if (req.user.guest || !req.adminAuth) {
+    console.error(chalk.redBright("Not Authorized."));
+    res.status(401).redirect("/");
+  } else {
+    User.findAll({ include: [{ model: Order }] })
+      .then(users => res.status(200).send(users))
+      .catch(err => {
+        res.status(404);
+        console.error(chalk.redBright("Could not retrive Users."));
+        next(err);
+      });
+  }
 });
 
 app.get("/:id", (req, res, next) => {
   const { id } = req.params;
-  User.findOne({ where: { id }, include: [{ model: Order }] })
-    .then(user => res.status(200).send(user))
-    .catch(err => next(err));
+  if ((!req.user || !req.adminAuth) && req.user.id !== id) {
+    console.error(chalk.redBright("Not Authorized."));
+    res.status(401).redirect("/");
+  } else {
+    User.findOne({ where: { id }, include: [{ model: Order }] })
+      .then(user => res.status(200).send(user))
+      .catch(err => {
+        res.status(404);
+        console.error(chalk.redBright("Could not retrieve User."));
+        next(err);
+      });
+  }
 });
 
 app.post("/", (req, res, next) => {
-  User.create(req.body)
-    .then(item => res.status(201).send(item))
-    .catch(err => next(err));
+  const { firstName, lastName, email, password } = req.body;
+  User.create({ firstName, lastName, email, password })
+    .then(user => res.status(201).send(user))
+    .catch(err => {
+      console.error(chalk.redBright("Could not create new User."));
+      next(err);
+    });
 });
 
 app.delete("/:id", (req, res, next) => {
-  const { id } = req.params;
-  User.destroy({
-    where: {
-      id
-    }
-  })
-    .then(() => res.status(200).end())
-    .catch(err => next(err));
+  if (!req.adminAuth) {
+    console.error(chalk.redBright("Not Authorized."));
+    res.status(401).redirect("/");
+  } else {
+    const { id } = req.params;
+    User.destroy({
+      where: {
+        id
+      }
+    })
+      .then(() => res.status(200).end())
+      .catch(err => next(err));
+  }
 });
 
 app.put("/:id", (req, res, next) => {
   const { id } = req.params;
-  User.findByPk(id)
-    .then(foundUser => {
-      return foundUser.update(req.body);
-    })
-    .then(updatedUser => {
-      return res.status(201).send(updatedUser);
-    })
-    .catch(err => next(err));
+  if (req.user.id !== id || !req.adminAuth) {
+    console.error(chalk.redBright("Not Authorized."));
+    res.status(401).redirect("/");
+  } else {
+    User.findByPk(id)
+      .then(foundUser => {
+        return foundUser.update(req.body);
+      })
+      .then(updatedUser => {
+        return res.status(201).send(updatedUser);
+      })
+      .catch(err => {
+        console.error("Could not update User.");
+        next(err);
+      });
+  }
 });
 
 module.exports = app;
