f8674bfd566e2ae9d4156492a2d061e9a9c5e3e1
Hector DelaRosa <hdelaros@gmail.com>
2020-02-01 18:27:46 -0500

    Added code cleanup and added chalk

--- server/api/items.js
+++ server/api/items.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express.Router();
+const chalk = require("chalk");
 const { Item } = require("../db/index");
 
 const paginate = (page, resultPerPage) => {
