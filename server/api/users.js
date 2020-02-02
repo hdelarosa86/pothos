6adb226718b3ea81a9e5165bb39829da0a2811df
anddyf <ferreiraanddy@gmail.com>
2020-02-02 03:06:08 -0500

    login in and dashboard edits

--- server/api/users.js
+++ server/api/users.js
@@ -19,7 +19,6 @@ app.use((req, res, next) => {
   }
 });
 
-
 const paginate = (page, resultPerPage) => {
   return { limit: resultPerPage, offset: page * resultPerPage };
 };
@@ -29,37 +28,38 @@ app.get("/", (req, res, next) => {
     console.error(chalk.redBright("Not Authorized."));
     res.status(401).redirect("/");
   } else {
-  const { perPage, page, filter } = req.query;
-  if (page !== "undefined") {
-    const resultPerPage = perPage;
-    const { limit, offset } = paginate(page - 1, resultPerPage);
-    if (filter !== "undefined") {
-      User.findAndCountAll({
-        order: [[filter, "DESC"]],
-        limit,
-        offset
-      }).then(items => {
-        res.status(200).send(items);
-      });
+    const { perPage, page, filter } = req.query;
+    if (page !== "undefined") {
+      const resultPerPage = perPage;
+      const { limit, offset } = paginate(page - 1, resultPerPage);
+      if (filter !== "undefined") {
+        User.findAndCountAll({
+          order: [[filter, "DESC"]],
+          limit,
+          offset
+        }).then(items => {
+          res.status(200).send(items);
+        });
+      } else {
+        User.findAndCountAll({
+          limit,
+          offset
+        }).then(items => {
+          res.status(200).send(items);
+        });
+      }
     } else {
-      User.findAndCountAll({
-        limit,
-        offset
-      }).then(items => {
-        res.status(200).send(items);
-      });
-    }
-  } else {
-    if (filter !== "undefined") {
-      User.findAll({
-        order: [[filter, "DESC"]]
-      })
-        .then(items => res.status(200).send(items))
-        .catch(err => next(err));
-    } else {
-      User.findAll()
-        .then(items => res.status(200).send(items))
-        .catch(err => next(err));
+      if (filter !== "undefined") {
+        User.findAll({
+          order: [[filter, "DESC"]]
+        })
+          .then(items => res.status(200).send(items))
+          .catch(err => next(err));
+      } else {
+        User.findAll()
+          .then(items => res.status(200).send(items))
+          .catch(err => next(err));
+      }
     }
   }
 });
@@ -113,7 +113,8 @@ app.delete("/:id", (req, res, next) => {
 
 app.put("/:id", (req, res, next) => {
   const { id } = req.params;
-  if (req.user.id !== id || !req.adminAuth) {
+  console.log("session", req.user.id !== id, !req.adminAuth);
+  if (req.user.id !== id || (req.user.id !== id && !req.adminAuth)) {
     console.error(chalk.redBright("Not Authorized."));
     res.status(401).redirect("/");
   } else {
