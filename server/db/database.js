9fe4e4b133962c2ad0a8a9a3fad9495eeb291c79
Hector DelaRosa <hdelaros@gmail.com>
2022-06-03 23:26:17 -0400

    modified db config

--- server/db/database.js
+++ server/db/database.js
@@ -4,16 +4,20 @@ const pkg = require('../../package.json');
 
 //const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name;
 
-const dbName = pkg.name
+const dbName = pkg.name;
 
 console.log(chalk.yellow(`Opening database connection to ${dbName}`));
 
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
   {
-    dialect: 'postgres',
-    protocol: 'postgres',
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false, // This line will fix new error
+      },
+    },
   }
 );
 
